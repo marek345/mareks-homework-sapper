b940caf5b4ec93f3658ce2bf6e0c59f95dd69958
marek345 <65684378+marek345@users.noreply.github.com>
2020-06-28 19:31:06 +0200

    Initial commit

---
+++ src/components/Joke.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount } from "svelte";
+    export let category = "random";
+    const apiURL = "https://api.chucknorris.io/jokes/random?category="+category;
+    let data = [];
+
+    onMount(async function() {
+        const response = await fetch(apiURL);
+        data = await response.json();
+    });
+
+</script>
+
+
+<p>{data.value}</p>
---
+++ src/routes/science.svelte
@@ -0,0 +1,13 @@
+<script>
+import Joke from '../components/Joke.svelte';
+</script>
+
+
+<svelte:head>
+	<title>Chuck Norris Jokes</title>
+</svelte:head>
+
+<h1>Jokes about science</h1>
+
+
+<Joke category="science"/>
