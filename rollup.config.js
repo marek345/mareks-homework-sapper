d04d84ffd49022d05723344cfc8a66b3ea069bfa
marek345 <65684378+marek345@users.noreply.github.com>
2020-06-30 09:09:20 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -22,7 +22,7 @@ export default {
 		plugins: [
 			replace({
 				...sapperEnv(),
-				'process.browser': true,
+				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
 			svelte({
@@ -93,7 +93,7 @@ export default {
 		plugins: [
 			resolve(),
 			replace({
-				'process.browser': true,
+				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
 			}),
 			commonjs(),
